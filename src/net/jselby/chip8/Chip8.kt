6d9990f3db4f608f1d35caf52a26535add68aaf6
James <jselby@jselby.net>
2016-04-10 14:20:52 +1000

    Initial commit

---
+++ src/net/jselby/chip8/Chip8.kt
@@ -0,0 +1,267 @@
+package net.jselby.chip8
+
+import javafx.animation.AnimationTimer
+import javafx.application.Application
+import javafx.application.Platform
+import javafx.scene.Scene
+import javafx.scene.canvas.Canvas
+import javafx.scene.canvas.GraphicsContext
+import javafx.scene.control.*
+import javafx.scene.image.WritableImage
+import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
+import javafx.stage.Modality
+import javafx.stage.Stage
+import net.jselby.chip8.decoder.decodeInstruction
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
+import kotlin.concurrent.thread
+
+/**
+ * A Chip8 emulator, written using Kotlin/JavaFX.
+ */
+class Chip8 : Application() {
+    val screenScale = 8.0
+
+    private val xResolution = 64.0 * screenScale
+    private val yResolution = 32.0 * screenScale
+
+    private val pixels = BooleanArray(64 * 32)
+
+    //private val renderRequests = ConcurrentLinkedQueue<DrawRequest>()
+    private var renderRequestSent = false
+
+    private lateinit var canvasRenderer: GraphicsContext
+
+    private lateinit var disPane: TextArea
+
+    var isVisible = true
+        private set
+
+    val keyMap = HashMap<Char, Int>()
+
+    private val img = WritableImage(xResolution.toInt(), yResolution.toInt())
+
+    override fun start(stage: Stage) {
+        println("CHIP-8 Emulator")
+
+        // Init keymap
+
+
+        stage.title = "CHIPy"
+        stage.isResizable = false
+
+        // Build menubar
+        val fileMenu    = Menu("File")
+        val optionsMenu = Menu("Options")
+        val helpMenu    = Menu("Debug")
+
+        val menuBar = MenuBar()
+        menuBar.menus.addAll(fileMenu, optionsMenu, helpMenu)
+
+        // Build renderer
+        val canvas = Canvas(xResolution, yResolution)
+        canvasRenderer = canvas.graphicsContext2D
+        canvasRenderer.fill = Color.BLACK
+        canvasRenderer.fillRect(0.0, 0.0, xResolution, yResolution)
+
+        // Build screen
+        val root = VBox()
+        root.children.addAll(menuBar, canvas)
+
+        //stage.width = xResolution
+        //println(menuBar.height)
+        //stage.height = yResolution + menuBar.height
+
+        stage.scene = Scene(root)
+
+        // Build disassembler view
+        disPane = TextArea()
+        disPane.style = "-fx-font-family: monospace"
+
+        val disStage = Stage()
+        disStage.scene = Scene(disPane, 480.0, 360.0)
+        disStage.initModality(Modality.WINDOW_MODAL)
+        disStage.title = "Disassembler"
+        disStage.minWidth = 480.0
+        disStage.minHeight = 360.0
+
+        // Build buttons
+        val disMenuItem = MenuItem("Disassembler")
+        disMenuItem.setOnAction {
+            disStage.show()
+        }
+        helpMenu.items.add(disMenuItem)
+
+        // Add callbacks
+        stage.setOnHidden {
+            isVisible = false
+        }
+
+        stage.show()
+
+        // Start renderer thread
+        AnimationCaller(this).start()
+
+        println("Ready to begin emulation!")
+
+        thread {
+            start(this)
+        }.name = "CHIP-8 Emulation Thread"
+    }
+
+    fun render() {
+        // Copy to our array and render
+        //println("Started render.")
+
+        println("Begin draw")
+
+        canvasRenderer.fill = Color.GREEN
+        canvasRenderer.fill()
+
+        println("Begin draw img")
+        canvasRenderer.drawImage(img, 0.0, 0.0)
+
+        println("End draw img")
+        /*var rendered = 0
+        var request = renderRequests.poll()
+        while(request != null) {
+
+            //println("${request.baseX} : ${request.baseY} : ${request.width} : ${request.height}")
+            for (y in 0..request.height - 1) {
+                val renderY = request.baseY + y
+                for (x in 0..request.width - 1) {
+                    val localIndex = y * request.width + x
+
+                    if (request.pixels[localIndex]) {
+                        val renderX = request.baseX + x
+
+                        val globalIndex = (renderY * 64 + renderX).toInt()
+
+                        pixels[globalIndex] = !pixels[globalIndex]
+
+                        //println("Painting $renderX $renderY with $x and $y base from $localIndex : $globalIndex")
+
+                        canvasRenderer.fill = if (pixels[globalIndex]) Color.WHITE else Color.BLACK
+                        canvasRenderer.fillRect(renderX * screenScale, renderY * screenScale,
+                                screenScale, screenScale)
+                    }
+                }
+            }
+
+            rendered++
+            request = renderRequests.poll()
+        }*/
+
+        //println("Completed render.")
+    }
+
+    fun postDrawRequest(request: DrawRequest) {
+        //renderRequests.add(request)
+
+        // Write to main image
+        for (y in 0..request.height - 1) {
+            val renderY = request.baseY + y
+            for (x in 0..request.width - 1) {
+                val localIndex = y * request.width + x
+
+                if (request.pixels[localIndex]) {
+                    val renderX = request.baseX + x
+
+                    val globalIndex = (renderY * 64 + renderX).toInt()
+
+                    pixels[globalIndex] = !pixels[globalIndex]
+
+                    //println("Painting $renderX $renderY with $x and $y base from $localIndex : $globalIndex")
+
+                    val color = if (pixels[globalIndex]) Color.WHITE else Color.BLACK
+                    //canvasRenderer.fillRect(renderX * screenScale, renderY * screenScale,
+                    //        screenScale, screenScale)
+
+                    for (drawX in (renderX * screenScale).toInt() .. ((renderX + 1) * screenScale).toInt()) {
+                        for (drawY in (renderY * screenScale).toInt() .. ((renderY + 1) * screenScale).toInt()) {
+                            if (drawX < xResolution && drawY < yResolution) {
+                                img.pixelWriter.setColor(drawX, drawY, color)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+       /* if (!renderRequestSent) {
+            renderRequestSent = true
+            Platform.runLater {
+                render()
+                renderRequestSent = false
+            }
+        }*/
+    }
+
+    fun sendRAM(ram : ByteArray) {
+        Platform.runLater {
+            var contents = ""
+
+            for (i in 0 .. ram.size - 1 step 2) {
+                var line = ""
+
+                val highInst = ram[i].toInt() and 0xFF
+                val lowInst = ram[i + 1].toInt() and 0xFF
+                val instVal = (highInst shl 8) + lowInst
+                val inst = decodeInstruction(instVal)
+
+                line += "0x${toHex(i)}  ${inst?.name}"
+                line += " ".repeat(13 - line.length)
+
+                // Dissect parameters
+                if (inst != null) {
+                    var lastChar = '0'
+                    for (charI in 0..inst.pattern.length - 1) {
+                        if (inst.pattern[charI] == lastChar) {
+                            continue
+                        }
+
+                        lastChar = inst.pattern[charI]
+
+                        if (lastChar.isLetter() && !(65..70).contains(lastChar.toInt())) {
+                            // This is an argument!
+                            line += "$lastChar=${inst.matcher.getArgument(instVal, lastChar)} "
+                        }
+                    }
+                }
+
+                line += " ".repeat(30 - line.length) + "; ${inst?.description}\n"
+
+                contents += line
+            }
+
+            disPane.text = contents
+            disPane.requestLayout()
+        }
+    }
+
+    fun setInstLine(line : Int) {
+        Platform.runLater {
+            disPane.scrollTop = line * 8.0
+        }
+    }
+
+    fun beep() {
+        Platform.runLater {
+            //Toolkit.getDefaultToolkit().beep();
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(Chip8::class.java, *args)
+        }
+    }
+}
+
+class AnimationCaller(val chip8: Chip8) : AnimationTimer() {
+    override fun handle(now: Long) {
+        chip8.render()
+    }
+}
---
+++ src/net/jselby/chip8/Main.kt
@@ -0,0 +1,308 @@
+package net.jselby.chip8
+
+import net.jselby.chip8.decoder.InstructionType
+import net.jselby.chip8.decoder.decodeInstruction
+import java.io.FileInputStream
+
+
+fun toHex(value : Number, length : Int = 4) = String.format("%0" + length + "X", value)
+
+/**
+ * A simple implementation of a CHIP-8 machine in Kotlin.
+ *
+ * HW info taken from https://en.wikipedia.org/wiki/CHIP-8 and attached documentation. by cowgod.
+ */
+fun start(chip: Chip8) {
+    // Post logo
+    // CHIP
+    /*chip.postDrawRequest(DrawRequest(5, 5, 5, 15,
+            booleanArrayOf(
+                    false, true,  true,  false, true, false, true, false, true,  true, true,  false, true, true,  false,
+                    true,  false, false, false, true, false, true, false, false, true, false, false, true, false, true,
+                    true,  false, false, false, true, true,  true, false, false, true, false, false, true, true,  false,
+                    true,  false, false, false, true, false, true, false, false, true, false, false, true, false, false,
+                    false, true,  true,  false, true, false, true, false, true,  true, true , false, true, false, false
+            )
+    ))*/
+
+    // Load game data
+    val romIn = FileInputStream("roms/INVADERS")
+    val rom = romIn.readBytes()
+    romIn.close()
+
+    println("Loaded ROM successfully.")
+
+    // Build CPU
+    val cpu = CPU()
+
+    var pointer = 0x200
+
+    // Copy ROM into memory
+    System.arraycopy(rom, 0, cpu.ram, pointer, rom.size)
+
+    // Update the UI
+    chip.sendRAM(cpu.ram)
+
+    var sysTime = System.currentTimeMillis()
+
+    println("Starting emulation at 0x${toHex(pointer, 3)}...")
+
+    // Main loop
+    while(chip.isVisible) {
+        // Check timers
+        if (System.currentTimeMillis() - sysTime > 1000 / 60) {
+            // We are ready for a re-render!
+            if (cpu.registers.delayTimer > 0) {
+                cpu.registers.delayTimer--
+            }
+
+            if (cpu.registers.soundTimer > 0) {
+                chip.beep()
+                cpu.registers.soundTimer--
+            }
+
+            sysTime = System.currentTimeMillis()
+        }
+
+        // On a CHIP-8, the first 4 bits define a single hex digit corresponding to a
+        //  general area of operation.
+        // The remainder of the instruction is the arguments for the instruction
+        //  or form part of the instruction itself. Regardless, we split it here to get
+        //  the general gist of the command.
+        val highInst = cpu.ram[pointer].toInt() and 0xFF
+        val lowInst = cpu.ram[pointer + 1].toInt() and 0xFF
+        val instVal = (highInst shl 8) + lowInst
+        val inst = decodeInstruction(instVal)
+
+        //println("${toHex(highInst, 2)} ${toHex(lowInst, 2)} = " + inst)
+        // Send to UI
+        chip.setInstLine(pointer)
+
+        // Increment CPU status pointer
+        pointer += 2
+
+        // Interpret instruction
+        when (inst) {
+            InstructionType.LDB -> {
+                // 6xkk - Set Vx = kk.
+                val x =      inst.matcher.getArgument(instVal, 'x')
+                val newVal = inst.matcher.getArgument(instVal, 'k')
+
+                //println("v$x = $newVal")
+
+                cpu.registers.vX[x] = newVal
+            }
+            InstructionType.LD -> {
+                // Set I = nnn.
+                val newVal = inst.matcher.getArgument(instVal, 'n')
+
+                //println("I = $newVal")
+
+                cpu.registers.i = newVal.toShort()
+            }
+            InstructionType.SEVB -> {
+                // Skip next instruction if Vx = kk.
+                val x = inst.matcher.getArgument(instVal, 'x')
+                val compareVal = inst.matcher.getArgument(instVal, 'k')
+
+                //println("v$x ?= $compareVal")
+
+                if (cpu.registers.vX[x] == compareVal) {
+                    pointer += 2
+                }
+            }
+            InstructionType.SNEB -> {
+                // Skip next instruction if Vx != kk.
+                val x = inst.matcher.getArgument(instVal, 'x')
+                val compareVal = inst.matcher.getArgument(instVal, 'k')
+
+                //println("v$x ?!= $compareVal")
+
+                if (cpu.registers.vX[x] != compareVal) {
+                    pointer += 2
+                }
+            }
+            InstructionType.LDV -> {
+                // Set Vx = Vy.
+                val x = inst.matcher.getArgument(instVal, 'x')
+                val y = inst.matcher.getArgument(instVal, 'y')
+
+                //println("v$x = $vy")
+
+                cpu.registers.vX[x] = cpu.registers.vX[y]
+            }
+            InstructionType.SUB -> {
+                // Set Vx = Vy.
+                val x = inst.matcher.getArgument(instVal, 'x')
+                val y = inst.matcher.getArgument(instVal, 'y')
+
+                //println("v$x -= $vy, vF = borrow")
+
+                val result = Math.abs(cpu.registers.vX[y] - cpu.registers.vX[x])
+                cpu.registers.vX[0xF] = if (cpu.registers.vX[x] > cpu.registers.vX[y]) 1 else 0
+                cpu.registers.vX[x] = result
+            }
+            InstructionType.CALL -> {
+                // Call subroutine at nnn.
+                val newPointer = inst.matcher.getArgument(instVal, 'n')
+
+                //println("Calling subroutine at 0x${toHex(newPointer, 3)} from 0x${toHex(pointer, 3)}")
+
+                cpu.stack.push(pointer)
+                pointer = newPointer
+            }
+            InstructionType.RET -> {
+                // Return from a subroutine.
+                val newPointer = cpu.stack.pop()
+
+                //println("Exiting subroutine at 0x${toHex(newPointer, 3)} to 0x${toHex(pointer, 3)}")
+
+                pointer = newPointer
+            }
+            InstructionType.JP -> {
+                // Jump to location nnn.
+                val newPointer = inst.matcher.getArgument(instVal, 'n')
+
+                //println("Jumping to 0x${toHex(newPointer, 3)} from 0x${toHex(pointer, 3)}")
+
+                pointer = newPointer
+            }
+            InstructionType.RND -> {
+                // Set Vx = random byte AND kk.
+                val x = inst.matcher.getArgument(instVal, 'x')
+                val andValue = inst.matcher.getArgument(instVal, 'k')
+                val randomVal = cpu.random.nextInt(255)
+
+                //println("Storing random value $randomVal AND $andValue in v$x")
+
+                cpu.registers.vX[x] = randomVal and andValue
+
+            }
+            InstructionType.DRW -> {
+                // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+                val x = cpu.registers.vX[inst.matcher.getArgument(instVal, 'x')]
+                val y = cpu.registers.vX[inst.matcher.getArgument(instVal, 'y')]
+                val size = inst.matcher.getArgument(instVal, 'n')
+
+                //println("Draw of height $size at $x:$y")
+
+                // Sprite is 8x5, with each bit corresponding to a X value (true/false, on or off XORed)
+                val array = BooleanArray(size * 8)
+
+                for ((index, byte) in (cpu.registers.i.toInt() .. cpu.registers.i + size - 1).withIndex()) {
+                    for (i in 0..7) {
+                        array[index * 8 + (7 - i)] = ((cpu.ram[byte].toInt() shr i) and 0x1) == 1
+                    }
+                }
+                //println(array.toList())
+
+                // Send off the request
+                chip.postDrawRequest(DrawRequest(x, y, 8, size, array))
+                //Thread.sleep(100000)
+
+                // p = 738, i = 754, size = 0, x = 20, y = 0
+                // p = 744, i = 754, size = 0, x = 20, y = 0
+
+            }
+            InstructionType.LDS -> {
+                // Set I = location of sprite for digit Vx.
+                val x = cpu.registers.vX[inst.matcher.getArgument(instVal, 'x')]
+
+                println("LDS: $x")
+
+                cpu.registers.i = (x * 5).toShort()
+            }
+            InstructionType.ADDB -> {
+                // Set Vx = Vx + kk.
+                val x =      inst.matcher.getArgument(instVal, 'x')
+                val newVal = inst.matcher.getArgument(instVal, 'k')
+
+                //println("v$x += $newVal")
+
+
+                cpu.registers.vX[x] += newVal
+            }
+            InstructionType.SKNP -> {
+                // Skip next instruction if key with the value of Vx is not pressed.
+                // TODO
+
+                pointer += 2
+            }
+            InstructionType.LDDV -> {
+                // Set delay timer = Vx.
+                val x = inst.matcher.getArgument(instVal, 'x')
+
+                //println("delayTimer = v$x")
+
+                cpu.registers.delayTimer = cpu.registers.vX[x]
+            }
+            InstructionType.LDDS -> {
+                // Set sound timer = Vx.
+                val x = inst.matcher.getArgument(instVal, 'x')
+
+                //println("soundTimer = v$x")
+
+                cpu.registers.soundTimer = cpu.registers.vX[x]
+            }
+            InstructionType.ADDI -> {
+                // Set I = I + Vx.
+                val x = cpu.registers.vX[inst.matcher.getArgument(instVal, 'x')]
+
+                cpu.registers.i = (cpu.registers.i.toInt() + x).toShort()
+            }
+            InstructionType.LDVD -> {
+                // Set Vx = delay timer value.
+                val x = inst.matcher.getArgument(instVal, 'x')
+
+                //println("v$x = delayTimer")
+
+                cpu.registers.vX[x] = cpu.registers.delayTimer
+            }
+            InstructionType.AND -> {
+                // Set Vx = Vx AND Vy.
+                val x = inst.matcher.getArgument(instVal, 'x')
+                val y = inst.matcher.getArgument(instVal, 'y')
+
+                //println("v$x = v$x AND v$y")
+
+                cpu.registers.vX[x] = cpu.registers.vX[x] and cpu.registers.vX[y]
+            }
+            InstructionType.ADDV -> {
+                // Set Vx = Vx + Vy, set VF = carry.
+                val x = inst.matcher.getArgument(instVal, 'x')
+                val y = inst.matcher.getArgument(instVal, 'y')
+
+                //println("v$x = v$x + v$y, vF = carry")
+
+                val result = cpu.registers.vX[x] + cpu.registers.vX[y]
+
+                cpu.registers.vX[x] = result and 0xFF
+                cpu.registers.vX[0xF] = if (result > 255) 1 else 0
+            }
+            InstructionType.LDBC -> {
+                // Store BCD representation of Vx in memory locations I, I+1, and I+2.
+                val x = cpu.registers.vX[inst.matcher.getArgument(instVal, 'x')]
+
+                cpu.ram[cpu.registers.i.toInt()] = Math.floor(x / 100.0).toByte()
+                cpu.ram[cpu.registers.i.toInt() + 1] = Math.floor(x % 100 / 10.0).toByte()
+                cpu.ram[cpu.registers.i.toInt() + 2] = (x % 10).toByte()
+            }
+            InstructionType.LDMR -> {
+                // Read registers V0 through Vx from memory starting at location I.
+                val x = inst.matcher.getArgument(instVal, 'x')
+
+                //println("Copying memory from 0x${toHex(cpu.registers.i, 3)} to v0..v$x")
+
+                for (vVal in 0..x) {
+                    cpu.registers.vX[x] = cpu.ram[cpu.registers.i + vVal].toInt()
+                }
+            }
+            else -> {
+                error("Unhandled instruction: $inst")
+            }
+        }
+
+        // Sleep a bit
+        //Thread.sleep(1)
+    }
+}
